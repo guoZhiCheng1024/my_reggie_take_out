39fa40acaa1519c1e049aa85971aec1522500a0e
reggie_take_out <963339484@qq.com>
2022-11-21 20:56:32 +0800

    完成启用，禁用

--- src/main/java/com/GZC/reggie/config/WebMvcConfig.java
+++ src/main/java/com/GZC/reggie/config/WebMvcConfig.java
@@ -1,9 +1,15 @@
 package com.GZC.reggie.config;
 
+import com.GZC.reggie.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.cbor.MappingJackson2CborHttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -13,5 +19,16 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         log.info("静态配置文件加载...");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2CborHttpMessageConverter messageConverter =new MappingJackson2CborHttpMessageConverter();
+
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+
+        converters.add(0,messageConverter);
+
+
     }
 }
