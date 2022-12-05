b5b983758e87d5af3d865dcdcf0e16fa92cfeb13
reggie_take_out <963339484@qq.com>
2022-12-05 21:27:33 +0800

    利用MybatisPlus实现公共字段自动填充

---
+++ src/main/java/com/GZC/reggie/common/MyMetaObjecthandler.java
@@ -0,0 +1,16 @@
+package com.GZC.reggie.common;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+
+public class MyMetaObjecthandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+
+    }
+}
--- src/main/java/com/GZC/reggie/entity/Employee.java
+++ src/main/java/com/GZC/reggie/entity/Employee.java
@@ -19,8 +19,13 @@ public class Employee implements Serializable {
     private String sex;
     private String idNumber;//身份证号码
     private Integer status;
+
+    @TableField(fill = FieldFill.INSERT)   //插入时填充字段
     private LocalDateTime createTime;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE) // 插入和更新时填充字段
     private LocalDateTime updateTime;
+
     @TableField(fill = FieldFill.INSERT)
     private Long createUser;
 
